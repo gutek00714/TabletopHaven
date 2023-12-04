ba5f1066b5f72132f260dd9dcb518c30302d22ec
Arashayo <73334775+Arashayo@users.noreply.github.com>
2023-12-04 21:39:22 +0100

    Backend init

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import http from "node:http";
+import express from "express";
+
+import {test} from "./controllers/test";
+
+const PORT = 8080;
+
+const app = express();
+
+app.use(test);
+
+http.createServer(app).listen(PORT, () => {
+  console.log(`Server lisening on port ${PORT}`);
+});
