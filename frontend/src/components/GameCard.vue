e52a9e27e1fbf8823a4aa7f1b967df87fad52526
Gustaw Łączkowski <guciolaczkowski1@gmail.com>
2024-01-24 15:37:37 +0100

    gamecard component + top5 on landing page

---
+++ frontend/src/components/GameCard.vue
@@ -0,0 +1,111 @@
+<template>
+  <div v-if="loading" class="game-card">
+    Loading game details...
+  </div>
+  <div v-else-if="error" class="game-card">
+    Error loading game details: {{ error }}
+  </div>
+  <div v-else class="game-card" @click="handleClick">
+    <div class="game-image-container">
+      <img :src="gameData.image" alt="Game image" class="game-image" />
+    </div>
+    <h2 class="game-title">{{ gameData.name }}</h2>
+    <h3 class="game-year">Year: {{ gameData.year }}</h3>
+    <div v-if="averageRating !== null">Rating: {{ averageRating.toFixed(1) }}</div>
+    <div>Players: {{ gameData.min_players }} - {{ gameData.max_players }}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  props: {
+    gameId: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      gameData: {},
+      loading: false,
+      error: null,
+      averageRating: null,
+    };
+  },
+  async created() {
+    this.fetchGameData();
+  },
+  methods: {
+    async fetchGameData() {
+      this.loading = true;
+      try {
+        const response = await axios.get(`http://localhost:3000/game/${this.gameId}`);
+        this.gameData = response.data;
+        this.calculateAverageRating();
+        this.error = null;
+      } catch (error) {
+        console.error("Error fetching game data:", error);
+        this.error = error.message || "An error occurred";
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    calculateAverageRating() {
+      if (this.gameData.rating && this.gameData.rating.length > 0) {
+        const sum = this.gameData.rating.reduce((a, b) => a + b, 0);
+        this.averageRating = sum / this.gameData.rating.length;
+      } else {
+        this.averageRating = null;
+      }
+    },
+
+    handleClick() {
+      // add button logic
+    },
+  },
+};
+</script>
+
+<style scoped>
+.game-card {
+    width: 200px; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    word-wrap: break-word;
+    cursor: pointer;
+    transition: transform 0.3s ease;
+}
+
+.game-card:hover {
+  background-color: #322f46;
+}
+
+.game-image-container {
+    width: 200px;
+    height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+}
+
+.game-card img {
+    max-width: 100%;
+    max-height: 100%;
+}
+
+.game-title {
+    font-size: 30px; 
+    margin-top: 0;
+}
+
+.game-year {
+  font-size: 20px;
+  margin-top: -10px;
+}
+</style>
--- frontend/src/components/RightContent.vue
+++ frontend/src/components/RightContent.vue
@@ -1,45 +1,52 @@
-<!-- RightContent.vue -->
 <template>
-    <div class="col m9 column-background materialize-red main-content">
-        <div class="main-site">
-            <!-- Main site content here -->
-            <div class="section white-text">
-                <h5>News</h5>
-                <div class="col m6">dupa</div>
-                <div class="col m6">dupa1</div>
-            </div>
-            <div class="divider"></div>
-            <div class="section white-text">
-                <h5>Top 5</h5>
-                <div v-for="item in backendItems" :key="item.id" class="col m2">{{ item.name }}</div>
-            </div>
+    <div class="col m9 column-background main-content">
+      <div class="main-site">
+        <!-- Main site content here -->
+        <div class="section white-text">
+          <h5>News</h5>
+          <div class="col m6">dupa</div>
+          <div class="col m6">dupa1</div>
         </div>
+        <div class="divider"></div>
+        <div class="section white-text">
+          <h5>Top 5</h5>
+          <GameCard v-for="item in backendItems" :key="item.id" :gameId="item.id" class="game-card col m2" />
+        </div>
+      </div>
     </div>
   </template>
   
   <script>
+  import GameCard from './GameCard.vue';
+  
   export default {
-      name: 'RightContent',
-      data() {
-          return {
-              backendItems: [
-                  { id: 1, name: 'Placeholder1' },
-                  { id: 2, name: 'Placeholder2' },
-                  { id: 3, name: 'Placeholder3' },
-                  { id: 4, name: 'Placeholder4' },
-                  { id: 5, name: 'Placeholder5' }
-              ]
-              // W rzeczywistości, zamiast używać statycznych danych, pobrałbyś dane z backendu i przypisałbyś je do backendItems
-          };
-      },
+    name: 'RightContent',
+    components: {
+      GameCard,
+    },
+    data() {
+      return {
+        backendItems: [
+          { id: 1, name: 'Placeholder1' },
+          { id: 2, name: 'Placeholder2' },
+          { id: 3, name: 'Placeholder3' },
+          { id: 4, name: 'Placeholder4' },
+          { id: 5, name: 'Placeholder5' }
+        ]
+      };
+    },
   }
   </script>
   
   <style scoped>
-    .column-background.materialize-red.main-content {
-        margin-left: 2.5rem;
-        margin-top: 3rem;
-        border-radius: 5px;
-    }
+  .column-background.main-content {
+    margin-left: 2.5rem;
+    margin-top: 3rem;
+    border-radius: 5px;
+  }
+
+  .game-card {
+    margin: 47px;
+  }
   </style>
   
