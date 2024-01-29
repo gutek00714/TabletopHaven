96a8d5d1d7781aeece4ee18c8ce567724151c89a
Amarczak001 <marczakadam005@gmail.com>
2024-01-29 17:06:07 +0100

    code cleanup

--- frontend/src/components/LeftMenu.vue
+++ frontend/src/components/LeftMenu.vue
@@ -17,42 +17,41 @@
 </template>
   
 <script>
-  export default {
-    name: 'LeftMenu',
-    
-  }
-  </script>
+export default {
+  name: 'LeftMenu',
+}
+</script>
 
-  <style scoped>
-  .col{
-    margin-left: 0 !important;
-  }
-  .section h5 {
-    color: #fff;
-    text-shadow: 1px 1px 2px #000;
-    margin-bottom: 20px;
-  }
+<style scoped>
+.col{
+  margin-left: 0 !important;
+}
+.section h5 {
+  color: #fff;
+  text-shadow: 1px 1px 2px #000;
+  margin-bottom: 20px;
+}
 
-  .divider {
-    margin: 10px 0;
-  }
+.divider {
+  margin: 10px 0;
+}
 
-  .menu-button{
-    color: #808191;
-    text-shadow: 1px 1px 2px #000;
-    text-transform: capitalize;
-    width: 100%;
-    box-sizing: border-box;
-    transition: all 0.3s ease;
-    border-radius: 4px;
-    padding: 35px 20px;
-    font-size: 18px;
-    line-height: 0.1;
-  }
+.menu-button{
+  color: #808191;
+  text-shadow: 1px 1px 2px #000;
+  text-transform: capitalize;
+  width: 100%;
+  box-sizing: border-box;
+  transition: all 0.3s ease;
+  border-radius: 4px;
+  padding: 35px 20px;
+  font-size: 18px;
+  line-height: 0.1;
+}
 
-  .menu-button:hover{
-    background-color: #322f46;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.3);
-    transform: translateY(-2px);
-  }
+.menu-button:hover{
+  background-color: #322f46;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.3);
+  transform: translateY(-2px);
+}
 </style>
