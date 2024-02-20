b761bfe7e031011c85098e1cc8214b6c5d00735f
Gustaw Łączkowski <guciolaczkowski1@gmail.com>
2024-02-20 15:22:50 +0100

    Create groups view

--- frontend/src/components/UserGroups.vue
+++ frontend/src/components/UserGroups.vue
@@ -4,7 +4,9 @@
       <h4>User Groups</h4>
       <div class="row">
         <div v-for="group in groups" :key="group.id" class="group-card col m2" @click="goToGroupDetail(group.id)">
-          <div class="group-name">{{ group.name }}</div>
+          <router-link :to="`/group/${group.id}`" class="group-item">
+            <div class="group-name">{{ group.name }}</div>
+          </router-link>
         </div>
       </div>
     </div>
@@ -52,7 +54,34 @@ export default {
   /* Add more styles for your group card */
 }
 
+.group-item {
+  text-decoration: none;
+  color: inherit;
+}
+
 .group-name {
-  /* Style for group name */
+  margin: 0.5rem;
+  padding: 0.5rem;
+  border-radius: 10px;
+  background-color: #272538;
+  transition: all 0.3s ease;
+  box-shadow: 0px 6px 15px rgba(0, 0, 0, 0.5);
+  text-decoration: none;
+  color: #FFF;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100px;
+  width: 190px;
+  overflow: hidden; 
+  font-size:18px;
+}
+
+.group-name:hover {
+  background-color: #322f46;
+  box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.8);
+  transform: translateY(-2px);
 }
 </style>
