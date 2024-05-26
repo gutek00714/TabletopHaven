3eb25e017747c203c8391546df899648960adcb4
Dominik <djdominko@gmail.com>
2024-05-26 16:48:40 +0200

    index.ts cleanup shelfControllers splitted

---
+++ backend/src/shelfControllers/shelfRouters.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import addGameRouter from './addGame';
+import removeGameRouter from './removeGame';
+import addWishlistRouter from './addWishlist';
+import removeWishlistRouter from './removeFavorites';
+import addFavoriteRouter from './addFavorites';
+import removeFavoriteRouter from './removeFavorites';
+
+
+const router = Router();
+
+router.use(addGameRouter);
+router.use(removeGameRouter);
+router.use(addWishlistRouter);
+router.use(removeWishlistRouter);
+router.use(addFavoriteRouter);
+router.use(removeFavoriteRouter);
+
+export default router;
