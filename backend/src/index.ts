39923b3a255dc95f58cbd697ecb8956c7d156ba4
Adam Marczak <119507593+Amarczak001@users.noreply.github.com>
2024-05-29 14:52:57 +0200

    Revert "index.ts cleanup chatControllers"
    This reverts commit 9acb6923a01242d9d7c254acd1bce7679b40980c.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import http from 'http';
 import { Server as SocketIOServer } from 'socket.io';
 import express from "express";
+import session from 'express-session';
 import passport from 'passport';
 import './googleAuth/passport-setup';
 import cors from 'cors';
@@ -10,14 +11,29 @@ import gameRoutes from './gamesControllers/gameRoutes';
 import shelfRoutes from './shelfControllers/shelfRoutes';
 import userRoutes from './userControllers/userRoutes';
 import groupRoutes from './groupControllers/groupRoutes';
-import useSession from './db/useSession';
-import initializeSocketIO from './chatControllers/chatControllers';
 
+import pgSession from 'connect-pg-simple';
+import { Pool } from 'pg';
+import pool from './db/pool';
+
+require('dotenv').config();
+console.log('PostgreSQL connection settings:', {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+const path = require('path');
 const PORT = 3000;
 const app = express();
 
-
-
+const PgSession = pgSession(session);
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error('SESSION_SECRET is not set');
+}
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
@@ -30,12 +46,30 @@ const io = new SocketIOServer(server, {
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(useSession);
+
 app.use(cors({
   origin: 'http://localhost:8081', // Your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // allow these methods
   allowedHeaders: ['Content-Type', 'Authorization'] // allow these headers
+}));
+
+
+app.use(session({
+  store: new PgSession({
+    pool: new Pool({ // Use node-postgres Pool
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      port: parseInt(process.env.DB_PORT || '5432', 10)
+    }),
+    tableName: 'session' // optional, default is 'session'
+  }),
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days for example
 }));
 
 // Initialize Passport
@@ -51,90 +85,89 @@ app.use(gameRoutes);
 app.use(shelfRoutes);
 app.use(userRoutes);
 app.use(groupRoutes);
-initializeSocketIO(server);
-
-// async function getUserById(userId: number) {
-//   const query = `SELECT username FROM users WHERE id = $1;`;
-//   try {
-//     const result = await pool.query(query, [userId]);
-//     if (result.rows.length > 0) {
-//       return result.rows[0]; // returns the user object
-//     } else {
-//       return null; // user not found
-//     }
-//   } catch (error) {
-//     console.error('Error fetching user by ID:', error);
-//     throw error;
-//   }
-// }
-
-// async function saveMessage(groupId: number, userId: number, message: string) {
-//   const insertQuery = `
-//     INSERT INTO group_messages (group_id, user_id, message)
-//     VALUES ($1, $2, $3)
-//     RETURNING *;`; // Returning the inserted row for confirmation
-
-//   try {
-//     const result = await pool.query(insertQuery, [groupId, userId, message]);
-//     console.log('Message saved:', result.rows[0]);
-//     return result.rows[0]; // Return the saved message
-//   } catch (error) {
-//     console.error('Error saving message:', error);
-//     throw error; // Rethrow the error for upstream error handling
-//   }
-// }
 
 
-// io.on('connection', (socket) => {
-//   console.log('A user connected');
+async function saveMessage(groupId: number, userId: number, message: string) {
+  const insertQuery = `
+    INSERT INTO group_messages (group_id, user_id, message)
+    VALUES ($1, $2, $3)
+    RETURNING *;`; // Returning the inserted row for confirmation
 
-//   socket.on('joinGroup', async (groupId, userId) => {
-//     socket.join(groupId);
-//     console.log(`User ${userId} joined group ${groupId}`);
+  try {
+    const result = await pool.query(insertQuery, [groupId, userId, message]);
+    console.log('Message saved:', result.rows[0]);
+    return result.rows[0]; // Return the saved message
+  } catch (error) {
+    console.error('Error saving message:', error);
+    throw error; // Rethrow the error for upstream error handling
+  }
+}
 
-//     // Optionally, send chat history to the user
-//     const messages = await fetchGroupMessages(groupId);
-//     socket.emit('chatHistory', messages);
-//   });
+async function getUserById(userId: number) {
+  const query = `SELECT username FROM users WHERE id = $1;`;
+  try {
+    const result = await pool.query(query, [userId]);
+    if (result.rows.length > 0) {
+      return result.rows[0]; // returns the user object
+    } else {
+      return null; // user not found
+    }
+  } catch (error) {
+    console.error('Error fetching user by ID:', error);
+    throw error;
+  }
+}
 
-//   socket.on('sendMessage', async (message, groupId, userId) => {
-//     try {
-//       const savedMessage = await saveMessage(groupId, userId, message); // Save message to database
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('joinGroup', async (groupId, userId) => {
+    socket.join(groupId);
+    console.log(`User ${userId} joined group ${groupId}`);
+
+    // Optionally, send chat history to the user
+    const messages = await fetchGroupMessages(groupId);
+    socket.emit('chatHistory', messages);
+  });
+
+  socket.on('sendMessage', async (message, groupId, userId) => {
+    try {
+      const savedMessage = await saveMessage(groupId, userId, message); // Save message to database
       
-//       const user = await getUserById(userId);
-//       const username = user ? user.username : "Unknown User";
+      const user = await getUserById(userId);
+      const username = user ? user.username : "Unknown User";
   
-//       const messageWithUsername = { ...savedMessage, username };
+      const messageWithUsername = { ...savedMessage, username };
       
-//       io.to(groupId).emit('receiveMessage', messageWithUsername);
-//     } catch (error) {
-//       console.error('Error sending message:', error);
-//     }
-//   });
+      io.to(groupId).emit('receiveMessage', messageWithUsername);
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  });
   
 
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected');
-//   });
-// });
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
 
-// async function fetchGroupMessages(groupId: number) {
-//   const selectQuery = `
-//     SELECT gm.message, gm.timestamp, u.username
-//     FROM group_messages gm
-//     JOIN users u ON gm.user_id = u.id
-//     WHERE gm.group_id = $1
-//     ORDER BY gm.timestamp ASC;`; // Adjusted to include username from the users table
+async function fetchGroupMessages(groupId: number) {
+  const selectQuery = `
+    SELECT gm.message, gm.timestamp, u.username
+    FROM group_messages gm
+    JOIN users u ON gm.user_id = u.id
+    WHERE gm.group_id = $1
+    ORDER BY gm.timestamp ASC;`; // Adjusted to include username from the users table
 
-//   try {
-//     const result = await pool.query(selectQuery, [groupId]);
-//     console.log('Fetched messages for group:', groupId);
-//     return result.rows; // Returns an array of messages with usernames
-//   } catch (error) {
-//     console.error('Error fetching group messages:', error);
-//     throw error;
-//   }
-// }
+  try {
+    const result = await pool.query(selectQuery, [groupId]);
+    console.log('Fetched messages for group:', groupId);
+    return result.rows; // Returns an array of messages with usernames
+  } catch (error) {
+    console.error('Error fetching group messages:', error);
+    throw error;
+  }
+}
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
